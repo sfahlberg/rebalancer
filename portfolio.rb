8f2931c5c4c12284072ef7484ef76b0912cd9cd8
Samuel Fahlberg <sam.fahlberg@gmail.com>
2015-07-13 00:36:55 -0700

    vanguard creates portfolios

---
+++ portfolio.rb
@@ -0,0 +1,6 @@
+class Portfolio
+  def initialize(name, account_numbers)
+    @name = name
+    @account_numbers = account_numbers
+  end
+end
--- vanguard.rb
+++ vanguard.rb
@@ -1,105 +1,26 @@
 class Vanguard
   require 'csv'
   require_relative 'helpers'
+  require_relative 'portfolio'
   attr_reader :portfolios
 
   def initialize
-    @portfolios = get_accounts
+    @portfolios = get_portfolios
   end
 
-  def get_accounts
-    current_blob = []
-    CSV.foreach('data/ofxdownload.csv') do |line|
-      if line.length > 0 && line[0][0].is_letter? && current_blob.length > 0
-        check_if_trades(current_blob)
-        current_blob = []
-      end
-      current_blob << line
-    end
-    check_if_trades(current_blob)
+  def get_portfolios 
+    portfolios = []
+    CSV.foreach('user_data/portfolio_names.txt', headers: true) do |line|
+      name = line['Portfolio Name']
+      accounts = {}
 
-    i = 0
-    BLOBS.each do |blob|
-      i += 1
-      build_new_csvs(i, blob) 
-    end
-
-    names = ["Fund Account Number", "Account Number", "Account Number", "Account Number"]
-    (1..i).each do |idx|
-      run_blob(idx, names[idx - 1])
-    end
-
-    add_account_type
-  end
-  def build_new_csvs(filename, blob)
-    CSV.open("data/tmp/#{filename}.csv", 'w') do |csv_object|
-      blob.each do |row|
-        next if row.empty?
-        csv_object << row
-      end
-    end
-  end
-
-  FUNDS = {}
-
-  def run_blob(filename, identifier)
-    path = "data/tmp/#{filename}.csv"
-    CSV.foreach(path, headers: true) do |line|
-      account = line[identifier]
-      next if account == nil
-      current_account = {}
-
-      line.headers.each_with_index do |header, i|
-        next if header == nil
-        unless identifier == header
-          current_account[header] = line[i]
-        end
+      (1...line.length).each do |i| 
+        account_num = line["Account Number #{i}"]
+        accounts[account_num] = true
       end
 
-      FUNDS[account] ||= []
-      FUNDS[account] << current_account
+      portfolios << Portfolio.new(name, accounts)
     end
-
-    File.delete(path)
-  end
-
-  BLOBS = []
-  def check_if_trades(current_blob)
-    unless current_blob[0][1] == "Trade Date"
-      BLOBS << current_blob
-    end
-  end
-
-  def add_account_type
-    lines = []
-    File.readlines('user_data/account_names.txt').each do |line|
-      line = line.gsub!("\n","")
-      lines << line unless line.nil?
-    end
-
-    portfolios = {}
-    (0..lines.length - 2).step(2) do |i|
-      FUNDS[lines[i]].each do |investment| 
-        if investment["Fund Name"] == "Vanguard Prime Money Market Fund"
-          symbol = "MMA"
-          investment["Share Price"] = 1
-        else
-          symbol = investment["Symbol"]
-        end
-
-        #convert str into num
-        investment["Shares"] = investment["Shares"].to_f
-        investment["Total Value"] = investment["Total Value"].to_f
-        investment["Share Price"] = investment["Share Price"].to_f
-
-
-        tax_type = lines[i + 1]
-        portfolios[tax_type] ||= {}
-        portfolios[tax_type][symbol] = investment
-      end
-    end
-
     portfolios
   end
-
 end
