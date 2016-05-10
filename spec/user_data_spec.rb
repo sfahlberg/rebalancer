173f85048e974796593fc7a35c4260fced57b620
Samuel Fahlberg <sam.fahlberg@gmail.com>
2016-05-09 21:01:27 -0500

    fix user_data spec

--- spec/user_data_spec.rb
+++ spec/user_data_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe UserData do
         investments = [inv_a, inv_b, inv_c]
 
         output = current_data.create_portfolios(investments)
-        portfolio = Portfolio.new("traditional", {"0030-33333333333"=>true, "44444444"=>true}, [inv_a, inv_b]) 
+        portfolio = Portfolio.new("traditional", ["0030-33333333333", "44444444"], [inv_a, inv_b], 120) 
 
         expect(output[0].name).to eq(portfolio.name)
         expect(output[0].investments).to eq(portfolio.investments)
